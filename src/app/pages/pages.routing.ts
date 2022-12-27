82bad372f3259ca38c4d2e1f6bdeb225b99787e4
paul <alvaropaul07@hotmail.com>
2022-12-27 11:03:22 -0500

    Fin seccion 8

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -6,6 +6,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 const routes: Routes = [
     { 
@@ -14,10 +16,12 @@ const routes: Routes = [
         component: PagesComponent,
         children: [
             //{ path: '', component: DashboardComponent},
-            { path: '', component: DashboardComponent},
-            { path: 'progress', component: ProgressComponent},
-            { path: 'grafica1', component: Grafica1Component},
-            { path: 'account-settings', component: AccountSettingsComponent},
+            { path: '', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+            { path: 'progress', component: ProgressComponent, data: { titulo: 'ProgressBar' } },
+            { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Grafica #1' } },
+            { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajuste de cta' } },
+            { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
+            { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' } },
             /*no funciona rutas a partir de path especifico, se debe quitar comentario en app-routing.module
             y dejar vacio el path de la primera linea*/
             //{ path: '', redirectTo: '/dashboard', pathMatch: 'full'}
